83525855063c50c07201a67232fc36b2b536b785
mcandidomendoza <mcandidomendoza@ucmerced.edu>
2020-12-05 19:38:30 -0800

    First commit

---
+++ isnum.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+intregex='^[0-9]+$'
+if ! [[ $1 =~ $intregex ]]; then
+	echo "$1 is not an integer"
+else
+	echo "$1 is an integer"
+fi
---
+++ test100.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+if [ $1 -gt 100 ]; then
+echo "value > 100"
+fi
